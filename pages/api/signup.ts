5c149975a7b43a38fdf98df79299583bcaa0a74d
michael belete <it.michael.belete@gmail.com>
2021-10-16 19:17:19 +0300

    finished sign up api

---
+++ pages/api/signup.ts
@@ -0,0 +1,32 @@
+import { getSession } from "next-auth/client";
+import prisma from "../../lib/prisma"
+import * as bcrypt from "bcrypt";
+
+export default async function handle(req, res) {
+  const { fullName, email, password, companyName } = req.body;
+
+  const newCompany = await prisma.company.create({
+    data: {
+      name: companyName,
+    },
+  });
+
+//   const newUser = await createUser(name, email, password, result.id);
+
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = await prisma.user.create({
+      data: {
+          fullName: fullName,
+          email: email,
+          password: hashedPassword,
+          company: { connect: { id: newCompany?.id } },
+      }
+  });
+
+  res.json({
+    user: newUser,
+    company: newCompany,
+  });
+}
