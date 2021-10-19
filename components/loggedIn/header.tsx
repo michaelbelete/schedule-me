05e2d6eee94ecb5d415d21d4270f8e71763fc1ec
michael belete <it.michael.belete@gmail.com>
2021-10-19 21:05:33 +0300

    finished copy to clipboard

--- components/loggedIn/header.tsx
+++ components/loggedIn/header.tsx
@@ -1,17 +1,15 @@
 import React, { useRef, useState } from "react";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { User } from ".prisma/client";
+import { useRouter } from "next/router";
+import { userInfo } from "os";
 
 const LLandingHeader: React.FC<{ user: User }> = (props) => {
+    const router = useRouter();
+    const link = window.location;
+    const [value, setValue] = useState(`${link}booking/${props?.user.email}`);
+    const [copySuccess, setCopySuccess] = useState("copy");
 
-
-    const [copySuccess, setCopySuccess] = useState('Copy');
-    const textAreaRef = useRef(null);
-    function copyToClipboard(e) {
-        textAreaRef.current.select();
-        document.execCommand('copy');
-        e.target.focus();
-        setCopySuccess('Copied!');
-    };
 
     return (
         <div className="flex flex-col justify-center items-center px-36 mt-10 mb-5">
@@ -21,17 +19,12 @@ const LLandingHeader: React.FC<{ user: User }> = (props) => {
 
             <div className="mt-10">
                 <div className="flex flex-row gap-5 justify-center px-8 py-3 bg-white rounded-xl item-center">
-                    <div
-                        ref={textAreaRef}
-                        className="w-auto"
-                    >
-                    {`http://localhost:3000/${props?.user.email}`}
-                    </div>
-                    {
-                        <div>
-                            <button onClick={copyToClipboard}> {copySuccess}</button>
-                        </div>
-                    }
+                    <input value={value} disabled className="w-96"/>
+
+                    <CopyToClipboard text={value}
+                        onCopy={() => setCopySuccess("Copied")}>
+                        <span>{ copySuccess }</span>
+                    </CopyToClipboard>
                 </div>
                 <p></p>
             </div>
